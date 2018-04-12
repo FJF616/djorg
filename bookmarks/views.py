181450cf6a21d081640356a748121cef9e2dd829
FJF616 <franzjfernando@hotmail.com>
2018-04-12 13:25:46 -0700

    adds initial unstyled bookmarks form

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Bookmark
 
 def index(request):
+    if request.method == 'POST':
+        form = BookmarkForm(request.POST)
+        if form.is_valid():
+            form.save()
     context = {'bookmarks': Bookmark.objects.all()}
     return render(request, 'bookmarks/index.html', context)
 
