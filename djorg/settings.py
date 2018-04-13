0d24d7b8d9a5968859defb4f981d10c25cf5dd26
FJF616 <franzjfernando@hotmail.com>
2018-04-13 12:44:50 -0700

    changed settings

--- djorg/settings.py
+++ djorg/settings.py
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'bookmarks',
+    'gunicorn',
+    'whitenoise',
+    'python-decouple',
+    'psycopg2',
+    'pytz',
+    'dj-database-url',
+
     
 ]
 
@@ -50,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djorg.urls'
@@ -118,6 +126,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# https://docs.djangoproject.com/en/2.0/howto/static-files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
