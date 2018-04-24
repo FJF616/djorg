d2ff5673449ff216352cbe6e7431fd15347f9310
FJF616 <franzjfernando@hotmail.com>
2018-04-24 11:23:39 -0700

    adds unit tests, fixes base.html

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from uuid import uuid4
 
 # Create your models here.
@@ -6,7 +7,7 @@ class Bookmark(models.Model):
     id=models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=200, blank=True)
     notes = models.TextField("Notes", blank=True)
-    url = models.URLField("URL", unique=True)
+    url = models.URLField("URL", unique=True, blank=False)
     #can add category but we won't for now
     #created_at and last_modified fields are very useful for almost any project
     created_at = models.DateTimeField(auto_now_add=True)
--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from .models import Bookmark
+class BookmarkTestCase(TestCase):
+    def setUp(self):
+        Bookmark.objects.create(name="Noteless bookmark",
+        url="http://www.bookmarkless-in-seattle.com")
+        Bookmark.object.create(name="More note-worthy bookmark",
+        notes="This bookmark is worth writing a note about!",
+        url ="http://www.note=this.com")
+        pass
+
+        def test_retrieving_valid_bookmark(self):
+            """Test that a stored bookmark has correct values."""
+            noteless_bookmark = Bookmark.objects.get(name="Noteless bookmark")
+            self.assertEqual(noteless_bookmark.name, "Noteless bookmark")
+            self.assertEqual(noteless_bookmark.url, "http://www.noteless-in-seattle.com")
+            self.assertEqual(noteless_bookmark.notes,"")
+            noted_bookmark = Bookmark.objects.get(name="More note-worthy bookmark")
+            self.assertEqual(noteless_bookmark.name, "More note-worthy bookmark")
+            self.assertEqual(noteless_bookmark.url, "http://www.noteless-in-seattle.com")
+            self.assertEqual(noteless_bookmark.notes,"This bookmark is worth writing a note about!")
+        
+        def test_invalid_bookmarks(self):
+            """Ensure database enforces validity/constraints on Bookmarks."""
+            Bookmark.objects.create(name="Bookmark * without* URL")
+            Bookmark.objects.create(name=123, url=456)
+
+        def test_dupe_urls(self):
+            """Ensrue database enforces unique urls"""
+             with self.assertRaises(IntegrityError) as context:
+            Bookmark.objects.create(name="Bookmark 2",
+                                    url="http://www.example.com")
+            self.assertTrue('UNIQUE constraint failed' in context.exception) 
+
 
 # Create your tests here.
