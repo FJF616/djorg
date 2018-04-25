eef141281c29b8e052686a9424ce265324c5b009
FJF616 <franzjfernando@hotmail.com>
2018-04-25 02:42:05 -0700

    fixes models.py

--- imageuploader/models.py
+++ imageuploader/models.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
  
 # Create your models here.
 class Uploader(models.Model):
-    pic = models.FIleField(upload_to="images/")
+    pic = models.FileField(upload_to="images/")
     upload_date = models.DateTimeField(auto_now_add=True)
 
 #Form model
