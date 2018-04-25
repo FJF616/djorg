a6106796bae8ab77abe5e3214ae1b9df4ecc1170
FJF616 <franzjfernando@hotmail.com>
2018-04-24 22:37:30 -0700

    adds views.py, templates.py

--- imageuploader/views.py
+++ imageuploader/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from imageuploader.models import Uploader, UploadForm
+from django.http import HttpResponseRedirectfrom 
+from django.urls import reverse
 
 # Create your views here.
+def home(request):
+    if request.method=="POST":
+        img = UploadForm(request.POST, request.FILES)
+        if img.is_valid():
+            img.save()
+            return HttpResponseRedirect(reverse('imageupload'))
+    else:   
+        img=UploadForm()
+    images=Uploader.objects.all()
+    return render(request, 'home.html', {'form':img, 'images':images})
