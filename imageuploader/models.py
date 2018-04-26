8dd2ad58077799840c5c787127fa62d5f20181f3
FJF616 <franzjfernando@hotmail.com>
2018-04-25 21:29:37 -0700

    changed whole project around  since nothing  but problems working with imagefiles.  now there is a probelm when ever

--- imageuploader/models.py
+++ imageuploader/models.py
@@ -1,13 +1,13 @@
-from django.db import models
-from django.forms import ModelForm
+# from django.db import models
+# from django.forms import ModelForm
  
-# Create your models here.
-class Uploader(models.Model):
-    pic = models.ImageField(upload_to="media/")
-    upload_date = models.DateTimeField(auto_now_add=True)
+# # Create your models here.
+# class Uploader(models.Model):
+#     pic = models.ImageField(upload_to="media/")
+#     upload_date = models.DateTimeField(auto_now_add=True)
 
-#Form model
-class UploadForm(ModelForm):
-    class Meta:
-        model = Uploader
-        fields = ('pic',)
+# #Form model
+# class UploadForm(ModelForm):
+#     class Meta:
+#         model = Uploader
+#         fields = ('pic',)
