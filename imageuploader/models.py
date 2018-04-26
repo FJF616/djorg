c892fcccba2aa1779542557bb446b1bafc19ac7d
FJF616 <franzjfernando@hotmail.com>
2018-04-25 23:30:05 -0700

    fixes models.py

--- imageuploader/models.py
+++ imageuploader/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Profile(models.Model):
-    userId = models.IntergerField(default=1)
+    userId = models.IntegerField(default=1)
     title = models.CharField(default="Default Title", max_length = 200)
     body = models.TextField(default-"Body of post goes here")
     picture = models.ImageField(default = "default.png")
