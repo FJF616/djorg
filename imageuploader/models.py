58ec6da904b3630478bfb42158e0422e97538113
FJF616 <franzjfernando@hotmail.com>
2018-04-25 23:31:23 -0700

    ugh

--- imageuploader/models.py
+++ imageuploader/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Profile(models.Model):
     userId = models.IntegerField(default=1)
     title = models.CharField(default="Default Title", max_length = 200)
-    body = models.TextField(defaul="Body of post goes here")
+    body = models.TextField(default="Body of post goes here")
     picture = models.ImageField(default = "default.png")
 
     class Meta: 
